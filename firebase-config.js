852f92fbeb2441cfd9832473c91447f30d42bbc5
acertolivraria-ux <acerto.livraria@gmail.com>
2026-06-30 08:16:10 -0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.15.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxE43A8C5Fvy0jecQD-4iYmASrGgRtY6g",
+  authDomain: "consultaemails-55dc7.firebaseapp.com",
+  projectId: "consultaemails-55dc7",
+  storageBucket: "consultaemails-55dc7.firebasestorage.app",
+  messagingSenderId: "666632159596",
+  appId: "1:666632159596:web:ec61c881762692e1f37349"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
