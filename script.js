8dfdc52de78a7cddb794ff24d47d7f911dddbca2
acertolivraria-ux <acerto.livraria@gmail.com>
2026-06-30 07:46:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -43,3 +43,80 @@ document
 .getElementById("btnPesquisar")
 
 .addEventListener("click",pesquisar);
+function pesquisar(){
+
+const loja=document.getElementById("loja").value;
+
+const editora=document.getElementById("editora").value.toLowerCase();
+
+}
+const resultado=dados.find(item=>{
+
+return(
+
+item.lojas.includes(loja)
+
+&&
+
+(
+
+item.editora.toLowerCase()==editora
+
+||
+
+item.cnpj==editora
+
+)
+
+);
+
+});
+if(resultado){
+
+}
+let html="";
+
+resultado.emails.forEach(email=>{
+
+html+=`
+
+<div class="email">
+
+<span>${email}</span>
+
+<button onclick="copiar('${email}')">
+
+📋
+
+</button>
+
+</div>
+
+`;
+
+});
+html+=`
+
+<button onclick="copiarTodos()">
+
+📋 Copiar todos
+
+</button>
+
+`;
+function copiar(email){
+
+navigator.clipboard.writeText(email);
+
+alert("Copiado!");
+
+}
+function copiarTodos(){
+
+const texto=resultado.emails.join(";");
+
+navigator.clipboard.writeText(texto);
+
+alert("Todos os e-mails foram copiados.");
+
+}
